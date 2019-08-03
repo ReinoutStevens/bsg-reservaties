2a3a62c030b6a98383f5d5f8a97cbaf068ba732f
Reinout Stevens <reinout@crazygames.com>
2019-08-03 23:03:31 +0200

    basic calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Helmet } from 'react-helmet';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import TopBar from './Topbar/Topbar';
+import EventsCalendar from './EventsCalendar/EventsCalendar';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Helmet>
+      <CssBaseline />
+      <TopBar />
+      <Container>
+        <EventsCalendar />
+      </Container>
+    </>
   );
 }
 
